b51c968db109982aafd11dbc19ba4ef47f92a36e
Damien Tohin Doumer Kake <damientohin@gmail.com>
2020-02-21 07:23:43 +0100

    Initial wasm with xforms

---
+++ XamarinWebAssembly/XamarinWebAssembly/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinWebAssembly.Services;
+using XamarinWebAssembly.Views;
+
+namespace XamarinWebAssembly
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
